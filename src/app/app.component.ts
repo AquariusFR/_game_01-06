62437a7c2067d1de22e39abafbfccca17c1798ad
Unknown <frederic.nobre@gmail.com>
2017-02-20 02:57:14 +0100

    fix(display)
    affichage d'un text, youhou

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'app works!';
+
+  constructor(private elementRef: ElementRef) { }
+  ngAfterContentInit() {
+    getCtx(this.elementRef);
+  }
 }
+
+function getCtx(elementRef: ElementRef) {
+
+  var canvas = elementRef.nativeElement.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+  canvas.className = 'game__canvas';
+  canvas.width = document.body.clientWidth;
+  canvas.height =  document.documentElement.clientHeight;
+  //ctx.imageSmoothingEnabled = false; // future
+
+  ctx.fillStyle = "rgba(0,0,0,1)";
+  ctx.font = "12px Arial";
+  ctx.fillText('finish', 10, 10);
+  return ctx;
+} 
+
+function fullscreen() {
+  var el = document.getElementById('canvas');
+
+  if (el.webkitRequestFullScreen) {
+    el.webkitRequestFullScreen();
+  }
+}
+
+
