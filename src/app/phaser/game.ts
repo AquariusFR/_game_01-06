fa5a63ccb80b3044c157d49c02c9dde182754c95
Frederic Nobre <ext.frederic.nobre@letrot.fr>
2017-03-02 12:04:06 +0100

    fix(aide a la decision)
    tri sur date

--- src/app/phaser/game.ts
+++ src/app/phaser/game.ts
@@ -10,12 +10,13 @@ export class Game {
     private cursors: Phaser.CursorKeys;
     private placeholderSprite: Phaser.Sprite;
     private wasd: any;
+    private speed = 300;
 
 
     constructor() {
         let self = this;
 
-        this.phaserGame = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+        this.phaserGame = new Phaser.Game(600 * window.devicePixelRatio, 400 * window.devicePixelRatio, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
         function preload() {
             self.preload();
@@ -61,6 +62,7 @@ export class Game {
         this.placeholderSprite = this.phaserGame.add.sprite(this.phaserGame.world.centerX, this.phaserGame.world.centerY, 'placeholder');
 
         this.phaserGame.physics.enable(this.placeholderSprite, Phaser.Physics.ARCADE);
+        this.placeholderSprite.body.collideWorldBounds = true;
         this.scroller.addChild(this.placeholderSprite);
 
         this.scroller.start();
@@ -78,13 +80,13 @@ export class Game {
         this.timestamp = this.now;
 
 
-        if (this.placeholderSprite.body.velocity.x || this.placeholderSprite.body.velocity.y) {
+        if (false && this.placeholderSprite.body.velocity.x || this.placeholderSprite.body.velocity.y) {
 
-            var coeffX = this.startX ? (this.timestamp - this.startX) * 150 /1000 : 0;
-            var coeffY = this.startY ? (this.timestamp - this.startY) * 150 /1000 : 0;
-            
-            var velocityx = Math.abs(this.placeholderSprite.body.velocity.x)- coeffX;
-            var velocityy = Math.abs(this.placeholderSprite.body.velocity.y)- coeffY;
+            var coeffX = this.startX ? (this.timestamp - this.startX) * 150 / 1000 : 0;
+            var coeffY = this.startY ? (this.timestamp - this.startY) * 150 / 1000 : 0;
+
+            var velocityx = Math.abs(this.placeholderSprite.body.velocity.x) - coeffX;
+            var velocityy = Math.abs(this.placeholderSprite.body.velocity.y) - coeffY;
 
             velocityx = velocityx < 10 ? 0 : velocityx;
             velocityy = velocityy < 10 ? 0 : velocityy;
@@ -96,26 +98,34 @@ export class Game {
 
             this.placeholderSprite.body.velocity.x = velocityx;
             this.placeholderSprite.body.velocity.y = velocityy;
-            console.log(this.timestamp - this.startY + 'ms',coeffY, this.placeholderSprite.body.velocity.x, this.placeholderSprite.body.velocity.y);
+
+            if (this.placeholderSprite.body.velocity.x = 0) {
+                this.startX = 0;
+            }
+            if (this.placeholderSprite.body.velocity.y = 0) {
+                this.startY = 0;
+            }
         }
+        this.placeholderSprite.body.velocity.set(0);
 
 
         if (this.wasd.left.isDown) {
-            this.placeholderSprite.body.velocity.x = -150;
+            this.placeholderSprite.body.velocity.x = -this.speed;
             this.startX = Date.now();
         }
         else if (this.wasd.right.isDown) {
-            this.placeholderSprite.body.velocity.x = 150;
+            this.placeholderSprite.body.velocity.x = this.speed;
             this.startX = Date.now();
             //// Define your actionsvar ACTIONS = {    LEFT: 1,    UP: 2,    RIGHT: 3,    DOWN: 4,    ATTACK: 5,    BASIC_ATTACK: 6,};// Define your keymap, as many keys per action as we wantvar defaultKeymap = {    [ACTION.LEFT]:  [Phaser.KeyCode.A, Phaser.KeyCode.LEFT],    [ACTION.UP]:    [Phaser.KeyCode.W, Phaser.KeyCode.UP],    [ACTION.RIGHT]: [Phaser.KeyCode.D, Phaser.KeyCode.RIGHT],    [ACTION.DOWN]:  [Phaser.KeyCode.S, Phaser.KeyCode.DOWN],    [ACTION.BASIC_ATTACK]: Phaser.KeyCode.CONTROL};// Create Keymap classvar Keymap = function( keyboard, defaultKeymap ) {    this.map = {};    var self = this;    _.forEach(defaultKeymap, function(KeyCode, action) {        self.map[action] = [];        if(_.isArray(KeyCode)) {            _.forEach(KeyCode, (code) => {                self.map[action].push(keyboard.addKey(code));            });        } else {            self.map[action].push(keyboard.addKey(KeyCode));        }    });};// isDown function for your actionKeymap.prototype.isDown = function(action) {    for(let i = 0, length = this.map[action].length; i < length; i++ ){        if(this.map[action][i].isDown) {            return true;        }    }    return false;};// Create the Keymapvar myMap = new Keymap(game.input.keyboard, defaultKeymap);// In your update function you can now useif( myMap.isDown(ACTION.LEFT) ) {    // do stuff}
 
         }
-        else if (this.wasd.up.isDown) {
-            this.placeholderSprite.body.velocity.y = -150;
+
+        if (this.wasd.up.isDown) {
+            this.placeholderSprite.body.velocity.y = -this.speed;
             this.startY = Date.now();
         }
         else if (this.wasd.down.isDown) {
-            this.placeholderSprite.body.velocity.y = 150;
+            this.placeholderSprite.body.velocity.y = this.speed;
             this.startY = Date.now();
         }
     }
