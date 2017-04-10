528bf9fb92cd8262fcea5dad925c5e2529f5a1e9
Frederic Nobre <ext.frederic.nobre@letrot.fr>
2017-04-10 11:39:36 +0200

    refactoring

--- src/app/phaser/layerToSprites.ts
+++ src/app/phaser/layerToSprites.ts
@@ -5,168 +5,31 @@ export class LayerToSprites {
 
 
     public placeMapSprite(layer: Phaser.TilemapLayer, map: Phaser.Tilemap, tilemapJson: any, group: Phaser.Group, game: Phaser.Game) {
-        let spriteData: any = _(tilemapJson.layers).find((l) => l.name == 'sprites'),
-            spritesInfo = _(spriteData.data)
-                .map((x, index) =>
-                    x > 0 ? <tileInfo>{ index: index, id: x } : null
-                )
-                .filter(x => x).value();
 
         let tilewidth = map.tileWidth,
             tileheight = map.tileHeight,
-            w = layer.width,
-            h = layer.height,
-            tileSprites = new Array<Array<tileInfo>>();
-        let rightTileIsSameGroup = (tileset: Phaser.Tileset, spriteInfo: {
-            index: any;
-            id: any;
-        }) => {
-            return spriteData.data[spriteInfo.index + 1] == spriteInfo.id + 1
-        }
-        let bottomTileIsSameGroup = (tileset: Phaser.Tileset, spriteInfo: {
-            index: any;
-            id: any;
-        }) => {
-            return spriteData.data[spriteInfo.index + 100] == spriteInfo.id + tileset.columns
+            layerJson = this.findLayerJson('sprites', tilemapJson),
+            layerData: Array<any>,
+            collectedSprites: collectedSpriteFromLayer;
+
+        if (!layerJson) {
+            return; // no data found
         }
 
-        let processedTiles = {},
-            tileSpriteFramesData = new Map<string, {
-                'frames': Array<frameAtlas>,
-                'image': any
-            }>();
+        collectedSprites = this.collectSpriteFromLayer(layerJson.data, map, tilewidth, tileheight);
 
-        for (var index = 0; index < spriteData.data.length; index++) {
+        this.updateTextureAtlasCache(collectedSprites, game);
+        this.addSpritesToGroup(collectedSprites, map, game, group);
 
-            let id = spriteData.data[index];
+        layer.renderable = false;
+    }
 
-            if (id == 0 || processedTiles[index]) {
-                continue;
-            }
-            let spriteInfo = {
-                index: index,
-                id: id
-            }
+    private addSpritesToGroup(collectedSprites: collectedSpriteFromLayer, map: Phaser.Tilemap, game: Phaser.Game, group: Phaser.Group) {
+        _(collectedSprites.tileSprites).each(tileSprite => {
 
-            // layerSprites
-            let row = Math.floor(spriteInfo.index / 100),
-                column = spriteInfo.index % 100;
-
-            let currentLayerSprite = new Array<{ index: number, id: number }>();
-
-            //currentLayerSprite.push(spriteInfo);
-
-            //checkIfTilesAround
-            let tilesRightId = spriteInfo.index,
-                tilesBottomId = spriteInfo.index;
-
-            let tileset = _(map.tilesets).find(t => t.containsTileIndex(spriteInfo.id));
-            let currentSpriteInfo = spriteInfo;
-
-            //currentLayerSprite.push(currentSpriteInfo);
-
-            let continueReadingRight = true,
-                continueReadingBottom = true,
-                spritewidth = 0,
-                spriteheight = 0;
-
-            while (continueReadingRight) {
-
-                spritewidth++;
-
-                let bottomSpriteInfo = {
-                    index: currentSpriteInfo.index,
-                    id: spriteData.data[currentSpriteInfo.index]
-                };
-                continueReadingBottom = bottomTileIsSameGroup(tileset, bottomSpriteInfo);
-
-                let currentspriteheight = 0
-
-                while (continueReadingBottom) {
-
-                    currentspriteheight++;
-                    currentLayerSprite.push(bottomSpriteInfo);
-
-                    processedTiles[bottomSpriteInfo.index] = true;
-
-                    continueReadingBottom = bottomTileIsSameGroup(tileset, bottomSpriteInfo);
-                    bottomSpriteInfo = {
-                        index: bottomSpriteInfo.index + 100,
-                        id: spriteData.data[bottomSpriteInfo.index + 100]
-                    }
-                }
-
-                if (currentspriteheight > spriteheight) {
-                    spriteheight = currentspriteheight
-                }
-
-                continueReadingRight = rightTileIsSameGroup(tileset, currentSpriteInfo);
-
-                currentSpriteInfo = {
-                    index: currentSpriteInfo.index + 1,
-                    id: spriteData.data[currentSpriteInfo.index + 1]
-                };
-            }
-
-
-            if (currentLayerSprite.length) {
-                let tilesetRaw: any = tileset,
-
-                    currentgid = currentLayerSprite[0].id - tileset.firstgid,
-                    tilex = tilesetRaw.drawCoords[currentgid * 2],
-                    tiley = tilesetRaw.drawCoords[1 + (currentgid * 2)],
-                    spritewidthPixel = spritewidth * tilewidth,
-                    spriteHeightPixel = spriteheight * tileheight,
-                    spriteFrameAtlas: frameAtlas = {
-
-                        filename: currentLayerSprite[0].id.toString(),
-                        frame: {
-                            x: tilex,
-                            y: tiley,
-                            w: spritewidthPixel,
-                            h: spriteHeightPixel
-                        },
-                        rotated: false,
-                        trimmed: false,
-                        spriteSourceSize: {
-                            x: tilex,
-                            y: tiley,
-                            w: spritewidthPixel,
-                            h: spriteHeightPixel
-                        },
-                        sourceSize: {
-                            w: spritewidthPixel,
-                            h: spriteHeightPixel
-                        }
-                    }
-
-                if (!tileSpriteFramesData.has(tileset.name)) {
-
-                    let newFrameAtlas = {
-                        frames: new Array<frameAtlas>(),
-                        image: tileset.image
-                    };
-
-                    tileSpriteFramesData.set(tileset.name, newFrameAtlas);
-                }
-
-
-                tileSpriteFramesData
-                    .get(tileset.name)
-                    .frames.push(spriteFrameAtlas);
-
-                tileSprites.push(currentLayerSprite);
-            }
-        }
-
-        tileSpriteFramesData.forEach((spriteFrameData, key) => {
-            game.cache.addTextureAtlas(key, '', spriteFrameData.image, spriteFrameData, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
-        });
-
-        _(tileSprites).each(tileSprite => {
-
-            let width = tilewidth,
-                height = tileheight, spriteInfo = tileSprite[0],
+            let width = map.tileWidth,
+                height = map.tileHeight,
+                spriteInfo = tileSprite[0],
                 row = Math.floor(spriteInfo.index / 100),
                 column = spriteInfo.index % 100,
                 x = Math.min(column * width, 100 * width),
@@ -175,8 +38,163 @@ export class LayerToSprites {
 
             game.add.sprite(x, y, tileset.name, spriteInfo.id.toString(), group);
         });
+    }
 
-        layer.renderable = false;
+    private updateTextureAtlasCache(collectedSprites: collectedSpriteFromLayer, game: Phaser.Game) {
+        collectedSprites.tileSpriteFramesData.forEach((spriteFrameData, key) => {
+            game.cache.addTextureAtlas(key, '', spriteFrameData.image, spriteFrameData, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+        });
+    }
+
+    private collectSpriteFromLayer(layerData: Array<any>, map: Phaser.Tilemap, tilewidth: number, tileheight: number): collectedSpriteFromLayer {
+        let processedTiles = new Map<number, boolean>(),
+            tileSprites = new Array<Array<tileInfo>>(),
+            tileSpriteFramesData = new Map<string, tileAtlas>();
+
+        _(layerData)
+            .each((id, index) => {
+
+                let spriteInfo: tileInfo,
+                    currentSpriteInfo: tileInfo,
+                    tileset: Phaser.Tileset,
+                    collectedLayerSprite: collectedSprite;
+
+                if (id == 0 || processedTiles.get(index)) {
+                    return;
+                }
+                spriteInfo = {
+                    index: index,
+                    id: id
+                };
+
+
+                tileset = _(map.tilesets).find(t => t.containsTileIndex(spriteInfo.id));
+                currentSpriteInfo = spriteInfo;
+
+                collectedLayerSprite = this.collectSprite(currentSpriteInfo, layerData, tileset, processedTiles);
+
+                this.addSpriteToLayerAtlas(collectedLayerSprite, tileSpriteFramesData, tileSprites, tileset, tilewidth, tileheight);
+
+            });
+        return {
+            tileSpriteFramesData: tileSpriteFramesData,
+            tileSprites: tileSprites
+        }
+    }
+
+    private findLayerJson(name: string, json: any) {
+        return _(json.layers).find((l) => l.name == 'sprites');
+    }
+
+    private rightTileIsSameGroup = (tileset: Phaser.Tileset, spriteInfo: tileInfo, layerData: any) => {
+        return layerData[spriteInfo.index + 1] == spriteInfo.id + 1
+    }
+    private bottomTileIsSameGroup = (tileset: Phaser.Tileset, spriteInfo: tileInfo, layerData: any) => {
+        return layerData[spriteInfo.index + 100] == spriteInfo.id + tileset.columns
+    }
+    private collectSprite(spriteInfo: tileInfo, layerData: any, tileset: Phaser.Tileset, processedTiles: Map<number, boolean>): collectedSprite {
+        let currentSpriteInfo = spriteInfo,
+            currentLayerSprite = new Array<{ index: number, id: number }>(),
+            continueReadingRight = true,
+            continueReadingBottom = true,
+            spritewidth = 0,
+            spriteheight = 0;
+
+        while (continueReadingRight) {
+
+            spritewidth++;
+
+            let bottomSpriteInfo: tileInfo = {
+                index: currentSpriteInfo.index,
+                id: layerData[currentSpriteInfo.index]
+            };
+            continueReadingBottom = this.bottomTileIsSameGroup(tileset, bottomSpriteInfo, layerData);
+
+            let currentspriteheight = 0
+
+            while (continueReadingBottom) {
+
+                currentspriteheight++;
+                currentLayerSprite.push(bottomSpriteInfo);
+
+                processedTiles.set(bottomSpriteInfo.index, true);
+
+                continueReadingBottom = this.bottomTileIsSameGroup(tileset, bottomSpriteInfo, layerData);
+                bottomSpriteInfo = {
+                    index: bottomSpriteInfo.index + 100,
+                    id: layerData[bottomSpriteInfo.index + 100]
+                }
+            }
+
+            if (currentspriteheight > spriteheight) {
+                spriteheight = currentspriteheight
+            }
+
+            continueReadingRight = this.rightTileIsSameGroup(tileset, currentSpriteInfo, layerData);
+
+            currentSpriteInfo = {
+                index: currentSpriteInfo.index + 1,
+                id: layerData[currentSpriteInfo.index + 1]
+            };
+        }
+        return {
+            layerSprite: currentLayerSprite,
+            spriteheight: spriteheight,
+            spritewidth: spritewidth
+        };
+    }
+    private addSpriteToLayerAtlas(collectedLayerSprite: collectedSprite, tileSpriteFramesData: Map<string, tileAtlas>, tileSprites: Array<Array<tileInfo>>, tileset: Phaser.Tileset, tilewidth: number, tileheight: number) {
+
+        let currentLayerSprite = collectedLayerSprite.layerSprite;
+
+        if (currentLayerSprite.length) {
+            let tilesetRaw: any = tileset,
+
+                currentgid = currentLayerSprite[0].id - tileset.firstgid,
+                tilex = tilesetRaw.drawCoords[currentgid * 2],
+                tiley = tilesetRaw.drawCoords[1 + (currentgid * 2)],
+                spritewidthPixel = collectedLayerSprite.spritewidth * tilewidth,
+                spriteHeightPixel = collectedLayerSprite.spriteheight * tileheight,
+                spriteFrameAtlas: frameAtlas = {
+
+                    filename: currentLayerSprite[0].id.toString(),
+                    frame: {
+                        x: tilex,
+                        y: tiley,
+                        w: spritewidthPixel,
+                        h: spriteHeightPixel
+                    },
+                    rotated: false,
+                    trimmed: false,
+                    spriteSourceSize: {
+                        x: tilex,
+                        y: tiley,
+                        w: spritewidthPixel,
+                        h: spriteHeightPixel
+                    },
+                    sourceSize: {
+                        w: spritewidthPixel,
+                        h: spriteHeightPixel
+                    }
+                }
+
+            if (!tileSpriteFramesData.has(tileset.name)) {
+
+                let newFrameAtlas = {
+                    frames: new Array<frameAtlas>(),
+                    image: tileset.image
+                };
+
+                tileSpriteFramesData.set(tileset.name, newFrameAtlas);
+            }
+
+
+            tileSpriteFramesData
+                .get(tileset.name)
+                .frames.push(spriteFrameAtlas);
+
+            tileSprites.push(currentLayerSprite);
+        }
     }
 }
 interface tileInfo {
@@ -203,4 +221,17 @@ interface frameAtlas {
         w: number,
         h: number
     }
+}
+interface tileAtlas {
+    frames: Array<frameAtlas>,
+    image: any
+}
+interface collectedSprite {
+    layerSprite: Array<tileInfo>,
+    spritewidth: number,
+    spriteheight: number
+}
+interface collectedSpriteFromLayer {
+    tileSprites: Array<Array<tileInfo>>,
+    tileSpriteFramesData: Map<string, tileAtlas>
 }
